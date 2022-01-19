c78b7b74f052deee9f762590c3ef839fdcf138aa
dawidplatek <82765796+dawidplatek@users.noreply.github.com>
2022-01-19 21:39:27 +0100

    Add CategoryController

--- src/main/java/com/to/wms/controller/CategoryController.java
+++ src/main/java/com/to/wms/controller/CategoryController.java
@@ -1,23 +1,52 @@
 package com.to.wms.controller;
 
+import com.to.wms.model.Category;
 import com.to.wms.service.CategoryService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 
 @RestController
-@RequestMapping(value = "/categories")
+@RequestMapping("api/v1/categories")
 public class CategoryController {
 
-    private CategoryService categoryService;
+    private final CategoryService categoryService;
 
     public CategoryController(CategoryService categoryService) {
         this.categoryService = categoryService;
     }
 
-    @GetMapping(value = "/get/word")
-    public String getAllCategories() {
-        return "test";
+    @GetMapping("/by-name")
+    public ResponseEntity<Category> getCategoryByName(@RequestParam String name) {
+        Category category = categoryService.getCategoryByName(name);
+        return ResponseEntity.ok(category);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<?>> getAllCategories() {
+        List<?> categories = categoryService.getAll();
+        return ResponseEntity.ok(categories);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Void> addCategory(@Valid @RequestBody Category category) {
+        categoryService.addCategory(category);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateCategory(@Valid @RequestBody Category categoryToUpdate, @PathVariable String id) {
+        categoryService.editCategory(id, categoryToUpdate);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCategory(@PathVariable String id) {
+        categoryService.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
