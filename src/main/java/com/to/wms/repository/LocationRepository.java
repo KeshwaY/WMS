cbc53ef4a06cc7f4415c992896b355f67601b1bf
dawidplatek <82765796+dawidplatek@users.noreply.github.com>
2022-01-19 20:53:44 +0100

    Add logic to service

--- src/main/java/com/to/wms/repository/LocationRepository.java
+++ src/main/java/com/to/wms/repository/LocationRepository.java
@@ -4,8 +4,12 @@ import com.to.wms.model.Location;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface LocationRepository extends MongoRepository<Location, String> {
     Location findLocationByShelf(String shelf);
+
+    List<Location> findAllLocationsByDepartmentName(String departmentName);
 }
--- src/main/java/com/to/wms/service/LocalisationService.java
+++ src/main/java/com/to/wms/service/LocalisationService.java
@@ -1,18 +1,24 @@
 package com.to.wms.service;
 
+import com.to.wms.model.Department;
 import com.to.wms.model.Location;
+import com.to.wms.repository.DepartmentRepository;
 import com.to.wms.repository.LocationRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class LocalisationService extends BasicGenericService<LocationRepository>{
 
     private final LocationRepository locationRepository;
+    private final DepartmentRepository departmentRepository;
 
-    public LocalisationService(LocationRepository genericLocationRepository, LocationRepository locationRepository) {
+    public LocalisationService(LocationRepository genericLocationRepository, LocationRepository locationRepository, DepartmentRepository departmentRepository) {
         super(genericLocationRepository);
         this.locationRepository = locationRepository;
+        this.departmentRepository = departmentRepository;
     }
 
     @Transactional(readOnly = true)
@@ -20,7 +26,14 @@ public class LocalisationService extends BasicGenericService<LocationRepository>
         return locationRepository.findLocationByShelf(shelf);
     }
 
-    public void addLocation(Location location) {
+    @Transactional(readOnly = true)
+    public List<Location> getAllLocationsByDepartment(String departmentName) {
+        return locationRepository.findAllLocationsByDepartmentName(departmentName);
+    }
+
+    public void addLocation(Location location, String departmentName) {
+        Department department = departmentRepository.findDepartmentByName(departmentName);
+        location.setDepartment(department);
         locationRepository.save(location);
     }
 
